1066638781eff817bbc456a2111ac8f196481e9a
Seba20146 <sebastian.gavrila@ulbsibiu.ro>
2023-12-05 11:24:52 +0200

    CRUD for cars

--- src/main/java/com/parking/parkinglot/ejb/CarsBean.java
+++ src/main/java/com/parking/parkinglot/ejb/CarsBean.java
@@ -3,6 +3,7 @@ package com.parking.parkinglot.ejb;
 
 import com.parking.parkinglot.common.CarDto;
 import com.parking.parkinglot.entities.Car;
+import com.parking.parkinglot.entities.User;
 import jakarta.ejb.EJBException;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
@@ -10,6 +11,7 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -38,5 +40,52 @@ private List<CarDto>copyCarsToDto(List<Car>cars){
         list.add(temp);
     }
     return list;
-}
+    }
+
+    public void createCar(String licensePlate,String parkingSpot,Long userId){
+        LOG.info("createCar");
+
+        Car car = new Car();
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        User user = entityManager.find(User.class,userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+
+        entityManager.persist(car);
+    }
+
+
+    public void updatedCar( Long cardId ,String licensePlate,String parkingSpot,Long userId){
+        LOG.info("updateCar");
+
+        Car car = entityManager.find(Car.class, cardId);
+        car.setLicensePlate(licensePlate);
+        car.setParkingSpot(parkingSpot);
+
+        User oldUser=car.getOwner();
+        oldUser.getCars().remove(car);
+
+
+        User user = entityManager.find(User.class,userId);
+        user.getCars().add(car);
+        car.setOwner(user);
+    }
+    public void deleteCarsByIds(Collection<Long> carIds) {
+        LOG.info("deleteCardsByIds");
+
+        for (Long carId : carIds) {
+            Car car= entityManager.find(Car.class, carId);
+            entityManager.remove(car);
+        }
+    }
+
+    public CarDto findById(Long id){
+
+        Car car = entityManager.find(Car.class, id);
+
+        return new CarDto(id, car.getLicensePlate(), car.getParkingSpot(), car.getOwner().getUsername());
+    }
+
 }
